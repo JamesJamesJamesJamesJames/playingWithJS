81411d2755437a9bafef20b616a7c8410a6ee592
JamesJamesJamesJamesJames <James_Moon@lcca.com>
2020-01-16 09:48:11 -0500

    js practice

---
+++ jsPractice.js
@@ -0,0 +1,6 @@
+var nameFirst = prompt("What is your first name?")
+var nameLast = prompt("What is your last name?")
+var userAge = prompt("How old are you?")
+
+console.log("Your full name is " + nameFirst + " " + nameLast)
+console.log("You are " + userAge + " years old")
