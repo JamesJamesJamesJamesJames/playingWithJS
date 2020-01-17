0d045971f4eacf1eeaf813084f4a89241e7f46e9
JamesJamesJamesJamesJames <James_Moon@lcca.com>
2020-01-17 08:33:12 -0500

    guessing game

---
+++ game.js
@@ -0,0 +1,21 @@
+// create secretNumber
+var secretNumber = 4;
+
+// ask user for guess
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
+// check guess
+if (guess === secretNumber) {
+    alert("YOU GOT IT RIGHT!");
+}
+
+// check if guess is higher
+else if (guess > secretNumber) {
+    alert("Too high. Guess again!");
+}
+
+// otherwise, check if lower
+else {
+    alert("Too low. Guess again!")
+}
+
